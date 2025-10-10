6b57f57512d54b656a20f16b8bf9388759b16808
Aznan <aznan9686@gmail.com>
2025-10-11 03:50:55 +0530

    changes in js using event listener method

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,49 @@
+// document.querySelector(".set").addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+
+var numberOfDrums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrums; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
+        alert("i got clicked!");
+    })
+
+}
+
+
+// document.querySelectorAll(".drum")[0].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+// document.querySelectorAll(".drum")[1].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+// document.querySelectorAll(".drum")[2].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+// document.querySelectorAll(".drum")[3].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+// document.querySelectorAll(".drum")[4].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+// document.querySelectorAll(".drum")[5].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+// document.querySelectorAll(".drum")[6].addEventListener("click", function () {
+//     alert("i got clicked!");
+// });
+
+
+
+
+// function handleClick() {
+//     alert("i got clicked!");
+// }
